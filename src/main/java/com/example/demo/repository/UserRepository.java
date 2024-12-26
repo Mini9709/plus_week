ed166a79ef04b5a4b405fed5f54d5b2a0be7a2eb
Mini9709 <tjdals9709@naver.com>
2024-12-26 18:31:21 +0900

    feat : db 접근 최소화 변경

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    @Modifying
+    @Query("Update User u Set u.status = 'PENDING' Where u.status = 'APROVED' and u.role = 'USER' and u.id in :userIds")
+    void updateAllUser(List<Long> userIds);
 }
