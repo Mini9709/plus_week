c16f43046e20267891fb41eeb6903a9a1dae6543
Mini9709 <tjdals9709@naver.com>
2024-12-26 18:30:39 +0900

    feat : test 환경 수정

--- src/test/java/com/example/demo/util/PasswordEncoderTest.java
+++ src/test/java/com/example/demo/util/PasswordEncoderTest.java
@@ -15,6 +15,27 @@ class PasswordEncoderTest {
         String testPassword = "password";
         String encodedPassword = PasswordEncoder.encode(testPassword);
 
+        assertNotNull(encodedPassword);
+        assertNotEquals(testPassword, encodedPassword);
         assertTrue(PasswordEncoder.matches(testPassword, encodedPassword));
+    }
+
+    @Test
+    void encodedPasswordsAreDifferent() {
+        String testPassword = "password";
+        String encodedPassword1 = PasswordEncoder.encode(testPassword);
+        String encodedPassword2 = PasswordEncoder.encode(testPassword);
+
+        assertNotEquals(encodedPassword1, encodedPassword2);
+    }
+
+    @Test
+    void notEqualsPassword() {
+        String testPassword1 = "password";
+        String testPassword2 = "password2";
+
+        String encodedPassword1 = PasswordEncoder.encode(testPassword1);
+
+        assertFalse(PasswordEncoder.matches(testPassword2, encodedPassword1));
     }
 }
